caea8948170a9df54f151d37b085f11bf5a9e003
pyaesonesoemoe <pyaesonesoemoe@my.jcu.edu.au>
2022-11-20 15:50:13 +0630

    Refactor password checker with functions

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,5 +1,16 @@
-password=input("Enter your password: ")
-while len(password)< 5:
-    print("Too short")
+def main():
+    password= get_password()
+    show_asterisks(password)
+
+def get_password():
     password = input("Enter your password: ")
-print(len(password)* "*")
+    while len(password) < 5:
+        print("Too short> Please enter again.")
+        password= input("Enter your password: ")
+    return password
+
+def show_asterisks(keywords):
+    print(len(keywords) * "*")
+
+main()
+
