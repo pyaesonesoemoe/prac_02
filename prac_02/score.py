b48447c307ed22c3bdee6533b601a086a4d43355
pyaesonesoemoe <pyaesonesoemoe@my.jcu.edu.au>
2022-11-20 18:55:55 +0630

    Added function for checking status of the score

--- prac_02/score.py
+++ prac_02/score.py
@@ -1,18 +1,17 @@
-
-
 def main():
     score = float(input("Enter score: "))
     print(check_status(score))
 
+
+
 def check_status(score):
     while 0<= score <= 100:
-            if score>90:
-                print("Excellent")
-            elif score>=50:
-                print("Passable")
-            else:
-                print("Bad")
-            score = float(input("Enter score: "))
+        if score>90:
+            return "Excellent"
+        elif score>=50:
+            return "Passable"
+        else:
+            return"Bad"
     else:
-        print("Invalid score")
+        return "Invalid score"
 main()
