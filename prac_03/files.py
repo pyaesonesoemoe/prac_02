19217ab0761f917cb8279d206d93052713aeca83
pyaesonesoemoe <pyaesonesoemoe@my.jcu.edu.au>
2022-12-04 22:39:16 +0800

    F string was added to income input

--- prac_03/files.py
+++ prac_03/files.py
@@ -1,3 +1,22 @@
 #1
 name= input("Enter your name: ")
 out_file=open("name.txt", "w")
+print(name, file= out_file)
+out_file.close()
+
+#2
+in_file=open("name.txt", "r")
+name=in_file.read()
+in_file.close()
+
+print(f"Your name is {name}")
+
+#3
+in_file=open("numbers.txt", "r")
+first_num=int(in_file.readline())
+second_num=int(in_file.readline())
+in_file.close()
+print(first_num + second_num)
+
+#4
+num_file=open("numbers.txt", "r")
--- prac_04/list_exercise.py
+++ prac_04/list_exercise.py
@@ -1,1 +1,7 @@
-]
+numbers=[5, 20, 1, 2, 3]
+for number in numbers:
+    print(f"Number: {number}")
+print("")
+
+print(f"The first number is {number[0]}")
+print(f"The last number is {number[1]}")
