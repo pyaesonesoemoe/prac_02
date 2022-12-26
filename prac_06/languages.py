331d03a299d949c3c23bb8d0406d24e0f7759441
pyaesonesoemoe <pyaesonesoemoe@my.jcu.edu.au>
2022-12-26 21:40:41 +0800

    ProgrammingLanguage class added

--- prac_06/languages.py
+++ prac_06/languages.py
@@ -1,3 +1,19 @@
+from prac_06.programming_language import ProgrammingLanguage
 
+def main():
+    python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
+    ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
+    visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
+    print(python)
+    print(ruby)
+    print(visual_basic)
+
+    languages=[python, ruby, visual_basic]
+    print("The dynamically typed languages are:")
+    ProgrammingLanguage.is_dynamic(python)
+    ProgrammingLanguage.is_dynamic(ruby)
+    ProgrammingLanguage.is_dynamic(visual_basic)
+
+main()
 
 
