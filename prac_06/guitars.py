23bef86d235942fdcc4053273862a48f7570f3c2
pyaesonesoemoe <pyaesonesoemoe@my.jcu.edu.au>
2022-12-27 09:25:16 +0800

    guitar program imported and the program for user to enter and add guitars is completed

---
+++ prac_06/guitars.py
@@ -0,0 +1,27 @@
+from prac_06.guitar import Guitar
+
+def main():
+    guitars= []
+    print("My guitars!")
+    name=input("Name: ")
+    while name!= "":
+        year=int(input("Year: "))
+        cost=int(input("Cost: $"))
+        guitar_to_add= Guitar(name, year, cost)
+        guitars.append(guitar_to_add)
+        print(guitar_to_add, " added.")
+        name=input("Name: ")
+
+    guitars.append(Guitar("Gibson L-5 CES", 1922, 16035.40))
+    guitars.append(Guitar("Line 6 JTV-59", 2010, 1512.9))
+
+    if guitars:
+        guitars.sort()
+        print("These are my guitars:")
+        for i, guitar in enumerate(guitars, 1):
+            vintage= ""
+            if guitar.is_vintage():
+                vintage= " (vintage)"
+            print(f"Guitar {i}: {guitar.name:>20} ({guitar.year}), worth ${guitar.cost:10,.2f}{vintage}")
+    else:
+        print("There are no guitars, buy one.")
